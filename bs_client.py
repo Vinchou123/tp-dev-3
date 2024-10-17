572e98834017f0a7f313ca227a7f4b08311aa60e
Vincenzo <vincent.luu@ynov.com>
2024-10-17 12:41:40 +0200

    TP4/dev

---
+++ bs_client.py
@@ -0,0 +1,19 @@
+import socket
+import sys
+
+
+host= '10.2.2.2'
+port = 13337
+
+s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect((host, port))
+
+s.sendall(b"Meooooo !")
+
+data=s.recv(1024)
+
+s.close
+print(f"Le serveru à répondu  {repr(data)}")
+
+exit(0)
