36ced325e9d335718972ca5241e2fa5bf1e865e6
Vincenzo <vincent.luu@ynov.com>
2024-10-23 19:21:58 +0200

    add utf-8 pour afficher correctement la réponse

--- bs_client.py
+++ bs_client.py
@@ -11,8 +11,10 @@ try:
     print(f"Connecté avec succès au serveur {host} sur le port {port}")
     
     message = input("Que souhaites-tu écrire au serveur ?")
-    s.sendall(message.encode())  
+    s.sendall(message.encode('utf-8'))  
     data = s.recv(1024)
+    
+    print('le serveur a répondu :'{data.decode('utf-8')})
     
 except Exception as e:
     print(f"Erreur lors de la connexion au serveur : {e}")
