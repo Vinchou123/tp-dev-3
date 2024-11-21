f4f554e5750b190ccbe28ebd005ffaff232758bf
Vincenzo <vincent.luu@ynov.com>
2024-11-21 10:03:41 +0100

    tP5

--- tp5_enc_server_2.py
+++ tp5_enc_server_2.py
@@ -2,7 +2,7 @@ import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.bind(('127.0.0.1', 9999))
+sock.bind(('0.0.0.0', 9999))
 sock.listen()
 client, client_addr = sock.accept()
 
