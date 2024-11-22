334857de5d592d9c0080f8ea230c442066fbeb4d
Vincenzo <vincent.luu@ynov.com>
2024-11-22 12:19:40 +0100

    chat server

--- chat_server_ii_2.py
+++ chat_server_ii_2.py
@@ -1,13 +1,12 @@
 import asyncio
 
 async def handle_client(reader, writer):
-
-   
+    addr = writer.get_extra_info('peername')
     client_ip, client_port = addr
 
     data = await reader.read(1024)
-    addr = writer.get_extra_info('peername')
     message = data.decode()
+
     print(f"Message du client ({client_ip}:{client_port}): {message}")
 
     response = f"Hello {client_ip}:{client_port}"
@@ -24,7 +23,6 @@ async def main():
     addr = server.sockets[0].getsockname()
     print(f'Serving on {addr}')
 
-    
     async with server:
         await server.serve_forever()
 
